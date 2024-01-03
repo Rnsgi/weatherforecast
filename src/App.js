3de4c51e5a93b250999be460be931efcb8805be5
rk33609 <Ramachandra.Kancharapu@cyient.com>
2024-01-03 10:35:04 +0530

    weatherforecast

--- src/App.js
+++ src/App.js
@@ -1,24 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Box, Container, Grid, Link, SvgIcon, Typography } from '@mui/material';
+import Search from './components/Search/Search';
+import FiveDaysForecast from './components/FiveDaysForecast/FiveDaysForecast';
+import { fetchWeatherData } from './api/OpenWeatherService';
+import { transformDateFormat } from './utilities/DatetimeUtils';
+import UTCDatetime from './components/Reusable/UTCDatetime';
+import LoadingBox from './components/Reusable/LoadingBox';
+import ErrorBox from './components/Reusable/ErrorBox';
+import { ALL_DESCRIPTIONS } from './utilities/DateConstants';
+import {
+  getTodayForecastWeather,
+  getWeekForecastWeather,
+} from './utilities/DataUtils';
 
 function App() {
+  const [weekForecast, setWeekForecast] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const searchChangeHandler = async (enteredData) => {
+    const [latitude, longitude] = enteredData.value.split(' ');
+
+    setIsLoading(true);
+
+    const currentDate = transformDateFormat();
+    const date = new Date();
+    let dt_now = Math.floor(date.getTime() / 1000);
+
+    try {
+      const [todayWeatherResponse, weekForecastResponse] =
+        await fetchWeatherData(latitude, longitude);
+
+      const all_week_forecasts_list = getWeekForecastWeather(
+        weekForecastResponse,
+        ALL_DESCRIPTIONS
+      );
+
+      setWeekForecast({
+        city: enteredData.label,
+        list: all_week_forecasts_list,
+      });
+    } catch (error) {
+      setError(true);
+    }
+
+    setIsLoading(false);
+  };
+
+  let appContent = (
+    <Box
+      xs={12}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        width: '100%',
+        minHeight: '500px',
+      }}
+    >
+    </Box>
+  );
+
+  if (weekForecast) {
+    appContent = (
+      <React.Fragment>
+        <Grid item xs={12} md={12}>
+          <FiveDaysForecast data={weekForecast} />
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  if (error) {
+    appContent = (
+      <ErrorBox
+        margin="3rem auto"
+        flex="inherit"
+        errorMessage="Something went wrong"
+      />
+    );
+  }
+
+  if (isLoading) {
+    appContent = (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          minHeight: '500px',
+        }}
+      >
+        <LoadingBox value="1">
+          <Typography
+            variant="h3"
+            component="h3"
+            sx={{
+              fontSize: { xs: '10px', sm: '12px' },
+              color: 'rgba(255, 255, 255, .8)',
+              lineHeight: 1,
+              fontFamily: 'Poppins',
+            }}
+          >
+            Loading...
+          </Typography>
+        </LoadingBox>
+      </Box>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      sx={{
+        maxWidth: { xs: '95%', sm: '80%', md: '1100px' },
+        width: '100%',
+        height: '100%',
+        margin: '0 auto',
+        padding: '1rem 0 3rem',
+        marginBottom: '1rem',
+        borderRadius: {
+          xs: 'none',
+          sm: '0 0 1rem 1rem',
+        },
+        boxShadow: {
+          xs: 'none',
+          sm: 'rgba(0,0,0, 0.5) 0px 10px 15px -3px, rgba(0,0,0, 0.5) 0px 4px 6px -2px',
+        },
+      }}
+    >
+      <Grid container columnSpacing={2}>
+        <Grid item xs={12}>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              width: '100%',
+              marginBottom: '1rem',
+            }}
+          >
+            
+            <UTCDatetime />
+            <Link
+              href="https://github.com/imaginnovate"
+              target="_blank"
+              underline="none"
+              sx={{ display: 'flex' }}
+            >
+            </Link>
+          </Box>
+          <Search onSearchChange={searchChangeHandler} />
+        </Grid>
+        {appContent}
+      </Grid>
+    </Container>
   );
 }
 
